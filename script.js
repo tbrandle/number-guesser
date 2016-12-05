b53172502f2fa8231b1fb86afd022ec9725ed0b9
Tim <tbrandle53@gmail.com>
2016-12-05 16:50:05 -0700

    Link JS file and added button/input var

---
+++ script.js
@@ -0,0 +1,4 @@
+var userGuess = document.getElementById('user-guess')
+var submitBtn = document.getElementById('submit-guess')
+var clearBtn = document.getElementById('clear')
+var resetBtn = document.getElementById('reset')
