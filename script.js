e5fd2deaf0a35c9c363677678fb97f3f88960f2d
Tim <tbrandle53@gmail.com>
2016-12-05 18:11:33 -0700

    Solved random number comparison

--- script.js
+++ script.js
@@ -2,4 +2,33 @@ var userGuess = document.getElementById('user-guess')
 var submitBtn = document.getElementById('submit-btn')
 var clearBtn = document.getElementById('clear-btn')
 var resetBtn = document.getElementById('reset-btn')
-var numberGuesser = document.querySelector('h1')
+var previousGuess = document.querySelector('.prev-guess')
+var randomNum = Math.floor(Math.random() * 100 + 1);
+
+
+// Displays the users last guess
+submitBtn.addEventListener('click', function () {
+  var parse = parseInt(userGuess.value);
+  previousGuess.innerText = userGuess.value;
+
+  if (randomNum === parse) {
+    console.log("Congrats!  You guessed the correct number!", randomNum);
+    }
+    else if (randomNum < parse){
+    console.log("You guessed too low...Try again", randomNum);
+    }
+    else {
+    console.log("Your guess was too high.", randomNum);
+  }
+}
+
+)
+
+//clears the user inputs
+
+
+
+
+
+
+// Math.floor(Math.random() * 100 + 1)
