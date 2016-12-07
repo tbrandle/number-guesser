a36fcf58fba3f3b1bdbf08ffba8f2b582cfeb74a
Tim <tbrandle53@gmail.com>
2016-12-07 14:47:55 -0700

    Add more css styling and layout of full page

--- script.js
+++ script.js
@@ -1,18 +1,57 @@
+//*************GLOBAL VARIABLE*************//
 var submitBtn = document.getElementById('submit-btn')
 var clearBtn = document.getElementById('clear-btn')
 var resetBtn = document.getElementById('reset-btn')
-var randomNum = Math.floor(Math.random() * 100 +1)
+var paramBtn = document.getElementById('param-btn')
 var userGuess = document.getElementById('user-guess')
 var lastGuess = document.querySelector('.last-guess')
+var randomNum = Math.floor((Math.random() * (maxFn() - minFn())) + minFn())
 
-var min = document.getElementById('min')
-var min = document.getElementById('max')
+//*************HELPER FUNCTIONS*************//
+
+//Set min and maxFn values and convert to integer
+  //global, paramBtn, resetBtn
+function minFn() {
+var min = document.getElementById('min').value
+return parseInt(min)
+}
+function maxFn() {
+var max = document.getElementById('max').value
+return parseInt(max)
+}
+
+//disables buttons
+  //userGuess, clearBtn, resetBtn
+function swtchBtn (val1, val2){
+  clearBtn.disabled = val1;
+  resetBtn.disabled = val2;
+}
+swtchBtn(true, true)
+
+//resets values and user inputs
+  //submitBtn, clearBtn, resetBtn,
+function eraseInput (var1, var2, var3){
+  var1.value = "";
+  var2.value = "";
+  var3.value = "";
+}
+function eraseText (var1, var2, var3){
+  var1.innerText = "";
+  var2.innerText = "";
+  var3.innerText = "";
+}
+//*************ADD EVENT LISTENERS*************//
+
+    //"Enter" button for min and max
+paramBtn.addEventListener('click', function(){
+  randomNum = Math.floor((Math.random() * (maxFn() - minFn())) + minFn())
+  console.log(randomNum);
+})
 
 submitBtn.addEventListener('click', function(){
-  var parse = parseInt(userGuess.value)
+  var parseG = parseInt(userGuess.value)
   var lgText = document.querySelector('.lg-text')
   var result = document.querySelector('.result')
-
   //displays last guess to p tag
   function guessDisplay (msg) {
     lgText.innerText = "Your last guess was:";
@@ -20,29 +59,26 @@ submitBtn.addEventListener('click', function(){
     result.innerText = msg;
   }
   //Sets specifications for user input, compares random number to user input
-  if (parse < 1 || parse > 100 || isNaN(parse) === true) { //<---isNaN(parse) is saying "parse is NaN"...that statement is equal to "true"
-    alert("ERROR: You must choose a number between 1-100");
-    userGuess.value = "";
+  if (parseG < minFn() || parseG > maxFn() || isNaN(parseG) === true) { //<---isNaN(parse) is saying "parseG is NaN"...that statement is equal to "true"
+    alert("ERROR: You must choose a number between the min and the max that you set");
+    eraseInput(userGuess);
     clearBtn.disabled = true
-  } else if (parse === randomNum) {
-    guessDisplay ("BOOM! Let's play again! Guess another number.")
-    randomNum = Math.floor(Math.random() * 100 +1)
-  } else if (parse < randomNum){
+  } else if (parseG === randomNum) {
+    guessDisplay ("BOOM! Let's make this more diffcult.");
+    var minTen = minFn() - 10;
+    var maxTen = maxFn() + 10;
+    randomNum = Math.floor((Math.random() * (maxTen - minTen)) + minTen);
+    console.log (minTen);
+    console.log (maxTen);
+    console.log (randomNum);
+  } else if (parseG < randomNum){
     guessDisplay ("That is too low")
   } else {
     guessDisplay ("That is too high")
   }
 })
 
-//disables buttons
-function swtchBtn (val1, val2){
-  clearBtn.disabled = val1;
-  resetBtn.disabled = val2;
-}
-swtchBtn(true, true)
-
 userGuess.addEventListener('input', function(){
-  swtchBtn(false, false)
   if (userGuess.value === "") {
     swtchBtn(true)
   } else {
@@ -50,20 +86,18 @@ userGuess.addEventListener('input', function(){
   }
 })
 
-//clears lastGuess without generating a new randomNum
 clearBtn.addEventListener('click', function(){
-  userGuess.value = ""
+  eraseInput(userGuess)
   swtchBtn(true)
 })
 
-//clears the userGuess, lastGuess and generates a new randomNum
 resetBtn.addEventListener('click', function () {
   var lgText = document.querySelector('.lg-text')
   var result = document.querySelector('.result')
-  userGuess.value = ""
-  lastGuess.innerText = ""
-  lgText.innerText = ""
-  result.innerText = ""
+  var min = document.getElementById('min')
+  var max = document.getElementById('max')
+  eraseInput(userGuess, min, max)
+  eraseText(lastGuess, lgText, result)
   swtchBtn(true, true)
-  randomNum = Math.floor(Math.random() * 100 +1)
+  randomNum = Math.floor((Math.random() * (maxFn() - minFn())) + minFn())
 })
