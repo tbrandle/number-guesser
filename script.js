e4821bba0499298089b2129212ab5dbec5b0d0aa
Tim <tbrandle53@gmail.com>
2016-12-06 17:27:45 -0700

    add normalize. JS working

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 var submitBtn = document.getElementById('submit-btn')
 var clearBtn = document.getElementById('clear-btn')
 var resetBtn = document.getElementById('reset-btn')
-var randomNum = Math.floor(Math.random() *100 +1)
+var randomNum = Math.floor(Math.random() * 100 +1)
 
 //compares random number to user input
 //displays last guess to p tag
-submitBtn.addEventListener('click', function () {
-  var userGuess = document.getElementById ('user-guess');
-  var parse = parseInt(userGuess.value);
-  var lastGuess = document.querySelector('.prev-guess');
-  lastGuess.innerText = userGuess.value;
+submitBtn.addEventListener('click', function(){
+  var userGuess = document.getElementById('user-guess')
+  var lastGuess = document.querySelector('.last-guess')
+  var parse = parseInt(userGuess.value)
+  var lgText = document.querySelector('.lg-text')
+  var result = document.querySelector('.result')
+  lgText.innerText = "Your last guess was:"
+  lastGuess.innerText = userGuess.value
 
-  if (randomNum === parse) {
-      console.log("Good job!", randomNum);
-    } else if (randomNum < parse) {
-      console.log("Too high", randomNum);
-    } else {
-      console.log("Too low", randomNum);
-    }
+
+  if (parse === randomNum) {
+    result.innerText = "You won!"
+  } else if (parse < randomNum){
+    result.innerText = "That is too low"
+  } else {
+    result.innerText = "That is too high"
   }
-)
-
-//clears the user inputs
-clearBtn.addEventListener('click', function() {
-  var userGuess = document.getElementById ('user-guess');
-
-  userGuess.value = "";
 })
 
-//Reset Button...this will clear the random number
-resetBtn.addEventListener('click', function() {
-  randomNum = Math.floor(Math.random() *100 +1);
-  var userGuess = document.getElementById ('user-guess');
-  var lastGuess = document.querySelector('.prev-guess');
-
-  userGuess.value = "";
-  lastGuess.innerText = "";
+//clears lastGuess without generating a new randomNum
+clearBtn.addEventListener('click', function(){
+  var userGuess = document.getElementById('user-guess')
+  userGuess.value = ""
 })
 
+//clears the userGuess, lastGuess and generates a new randomNum
+resetBtn.addEventListener('click', function () {
+  var userGuess = document.getElementById('user-guess')
+  var lastGuess = document.querySelector('.last-guess')
+  var lgText = document.querySelector('.lg-text')
+  var result = document.querySelector('.result')
+  userGuess.value = ""
+  lastGuess.innerText = ""
+  lgText.innerText = ""
+  result.innerText = ""
 
-
-
-
-// Math.floor(Math.random() * 100 + 1)
+  randomNum = Math.floor(Math.random() * 100 +1)
+})
